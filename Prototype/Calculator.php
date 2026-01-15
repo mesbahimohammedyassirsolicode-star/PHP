7349c2c7b333816c09f27d24f4df55a4deec1c9e
https://github.com/mesbahimohammedyassirsolicode-star <mesbahi.mohammedyassir.solicode@gmail.com>
2026-01-15 12:58:23 +0100

    add

---
+++ Prototype/Calculator.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculator</title>
+</head>
+<body>
+    <form action="" method="POST">
+        <label for="number1">number 1</label>
+        <input type="number" name="nb1" id="nb1">
+        <select name="operators" id="operators">
+            <option value="plus">+</option>
+            <option value="moins">-</option>
+            <option value="double">*</option>
+            <option value="div">/</option>
+        </select>
+        <label for="number2">number 2</label>
+        <input type="number" name="nb2" id="nb2">
+    </form>
+    <button type="calcul" name="calcul">calcul</button>
+    <?php 
+     ?>
+</body>
+</html>
--- 📘 Chapter 5 – Custom Functions and Variable Scope/execrice/form.php
+++ 📘 Chapter 5 – Custom Functions and Variable Scope/execrice/form.php
@@ -1,42 +1,65 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <title> Form</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 <body>
+    <form method="post">
+        <label for="nom">Name :</label><br>
+        <input id="nom" name="nom" type="text"><br>
+        <label for="age">Age :</label><br>
+        <input type="text" name="age" id="age"><br>
 
-<h1>Form (Name - Age - Gender)</h>
+        <label for="check">Genre :</label><br>
+        <input type="radio" name="gendre" id="check" value="male">Male
+        
+        <input type="radio" name="gendre" value="female">Female
 
-<form method="POST" action="">
-  <label>Name:</label><br>
-  <input type="text" name="name"><br><br>
+        <br><br>
+        <button type="submit" name= "send">Submit</button>
+    </form>
 
-  <label>Age:</label><br>
-  <input type="number" name="age"><br><br>
+<?php
+$errors="<ul>";
+if (isset($_POST["send"])){
 
-  <label>Gender:</label><br>
-  <input type="radio" name="gender" value="Male"> Male <br>
-  <input type="radio" name="gender" value="Female"> Female <br><br>
+    if (!empty ($_POST['nom']) && !empty ($_POST['age']) && !empty ($_POST['gendre']) && is_numeric ($_POST['age'])  ){
+    
+    $name = $_POST ["nom"];
+    $age = $_POST ["age"];
+    $gender = $_POST ["gendre"];
 
-  <button type="submit" name="submit">Send</button>
-</form>
+    $textGender="Ms";
 
-<hr>
-<?php 
-if (isset($_POST["submit"])){
-$name = $_POST["name"];
-$age = $_POST["age"];
-$gender = $_POST["gender"];
-if ($gender === "Male"){
-  echo "Mr $name <br> Age:$age " ;
+        if ($gender == "male"){
+         $textGender="Mr";
+
+    } 
+    echo "$textGender $name a $age";
+    }
+    else{
+       if(empty ($_POST['nom'])){
+        $errors.="<li>Name is empty !</li>";
+
+       }
+        if(empty ($_POST['age'])){
+        $errors.="<li>Age is empty !</li>";
+
+       }
+        if(empty ($_POST['gendre'])){
+        $errors.="<li>Gender is empty !</li>";
+
+       }
+        if(!is_numeric ($_POST['age'])){
+        $errors.="<li>Age is not a number !</li>";
+
+       }
+       $errors.="</ul>";
+       echo $errors;
+    }
 }
-else {
-  echo "Ms $name <br> age: $age";
-}
-}
- ?>
-
-
+?>
 </body>
 </html>
