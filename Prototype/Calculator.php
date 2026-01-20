bb6f80a6b6964e7c7c39d424479d4f3a09ac50b6
https://github.com/mesbahimohammedyassirsolicode-star <mesbahi.mohammedyassir.solicode@gmail.com>
2026-01-20 10:17:28 +0100

    add

--- Prototype/Calculator.php
+++ Prototype/Calculator.php
@@ -1,25 +1,80 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calculator</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Calculator</title>
 </head>
 <body>
-    <form action="" method="POST">
-        <label for="number1">number 1</label>
-        <input type="number" name="nb1" id="nb1">
-        <select name="operators" id="operators">
-            <option value="plus">+</option>
-            <option value="moins">-</option>
-            <option value="double">*</option>
-            <option value="div">/</option>
-        </select>
-        <label for="number2">number 2</label>
-        <input type="number" name="nb2" id="nb2">
-    </form>
-    <button type="calcul" name="calcul">calcul</button>
-    <?php 
-     ?>
+
+<form action="" method="POST">
+  <label>number 1</label>
+  <input type="number" name="nb1" id="nb1">
+
+  <select name="op" id="operators">
+    <option value="">--choose--</option>
+    <option value="plus">+</option>
+    <option value="minus">-</option>
+    <option value="multi">*</option>
+    <option value="div">/</option>
+  </select>
+
+  <label>number 2</label>
+  <input type="number" name="nb2" id="nb2">
+
+  <button type="submit" name="calcul">calcul</button>
+</form>
+
+<?php
+$errors = "<ul style='color:red;'>";
+// for calcul
+function calc($num1, $num2, $operator) {
+  if ($operator === "plus") {
+    return $num1 + $num2;
+  } elseif ($operator === "minus") {
+    return $num1 - $num2;
+  } elseif ($operator === "multi") {
+    return $num1 * $num2;
+  } elseif ($operator === "div") {
+    if ($num2 == 0) {
+      return "You can't divide by 0";
+    }
+    return $num1 / $num2;
+  }
+  return "Invalid operator";
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+  //  nb1 error
+  if (!isset($_POST["nb1"]) || $_POST["nb1"] === '') {
+    $errors .= "<li>number 1 is empty !</li>";
+  }
+
+  //  nb2 error
+  if (!isset($_POST["nb2"]) || $_POST["nb2"] === '') {
+    $errors .= "<li>number 2 is empty !</li>";
+  }
+
+  //  operator error
+  if (!isset($_POST["op"]) || $_POST["op"] === '') {
+    $errors .= "<li>operator is empty !</li>";
+  }
+
+  $errors .= "</ul>";
+
+  //  If errors exist
+  if ($errors !== "<ul style='color:red;'></ul>") {
+    echo $errors;
+  } else {
+    $num1 =  $_POST["nb1"];
+    $num2 =  $_POST["nb2"];
+    $operator = $_POST["op"];
+    $result = calc($num1, $num2, $operator);
+    echo "<h3>Result : $result</h3>";
+  }
+}
+?>
+
 </body>
 </html>
--- 📘 Chapter 3 – Loops for, while,foreach/loops.php
+++ 📘 Chapter 3 – Loops for, while,foreach/loops.php
@@ -1,22 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Library</title>
+  <style>
+    body {
+        font-family: Arial, sans-serif;
+        margin: 20px;
+        background-color: #f0f0f0;
+        display: grid;
+        justify-items: center;
+        grid-template-columns: auto auto auto;
+        row-gap: 15px;
+        column-gap: 15px;
+
+
+    }
+    .shelf {
+        border: 1px solid #000;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        padding: 10px;
+        margin: 10px 0;
+        width: 300px;
+        background-color: #f9f9f9;
+    }
+  </style>
+</head>
+<body>
+
 <?php
-// loop for
-for($i = 1; $i <= 10; $i++){
-    echo "nombre : $i <br>";
+  $livres = [
+
+    [
+
+        "titre" => "Apprendre PHP",
+
+        "auteur" => "Fatine Chebab",
+
+        "annee" => 2024,
+
+        "prix" => 120,
+
+        "disponible" => true
+
+    ],
+
+    [
+
+        "titre" => "JavaScript pour débutants",
+
+        "auteur" => "Ali Ahmed",
+
+        "annee" => 2023,
+
+        "prix" => 100,
+
+        "disponible" => false
+
+    ],
+
+    [
+
+        "titre" => "HTML & CSS",
+
+        "auteur" => "Sara Benali",
+
+        "annee" => 2022,
+
+        "prix" => 80,
+
+        "disponible" => true
+
+    ]
+
+
+];
+$total = 0;
+foreach ($livres as $livre) {
+    $shelf = "<div class='shelf'>";
+    $shelf .= "<p>Titre de livre : {$livre['titre']}</p>";
+    $shelf .= "<p>Auteur de livre : {$livre['auteur']}</p>";
+    $shelf .= "<p>Annee : {$livre['annee']}</p>";
+    $shelf .= "<p>Prix de livre : {$livre['prix']}</p>";
+    $shelf .=  $livre['disponible'] ? "Disponible" : "Non Disponible";
+    $shelf .= "</div>";
+    echo $shelf;
+    if ($livre['disponible']) {
+        $total += $livre['prix'];
+    }
 }
-//while loop
-$j =1;
-while ($j <= 5){
-    echo "itreation : $j <br>";
-    $j++;
-}
-// foreach loop
-$animaux = ["chat","chein","lapin"];
-foreach ($animaux as $animal){
-    echo"animal : $animal <br>";
-}
-// break and continue 
-for ($i= 1; $i <= 10;$i++){
-    if ($i==5) continue;
-    if($i == 8)break;
-    echo "valeur : $i <br>";
-}
+echo "<p>Total des livres disponibles : $total</p>";
+
+?>
+  
+</body>
+</html>
